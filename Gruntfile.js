f42a4423a879fbef661438e9b853cb44c7836c4f
Artem Volkhin <artem@volkhin.com>
2014-05-17 17:46:47 +0400

    grunt watch on specs

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,7 +41,7 @@ module.exports = function(grunt) {
         },
         watch: {
             scripts: {
-                files: "js/**/*.js",
+                files: ["js/**/*.js", "spec/*.js"],
                 tasks: ["jshint", "jasmine"],
                 options: {
                     spawn: false,
