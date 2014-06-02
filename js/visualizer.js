4d7b62a3a31207903c06b4821d8f3874ed8bd056
Artem Volkhin <artem@volkhin.com>
2014-06-02 10:58:06 +0400

    cleanup

--- js/visualizer.js
+++ js/visualizer.js
@@ -2,7 +2,6 @@ define(function(require) {
     "use strict";
 
     var $ = require("jquery"),
-        Intersection = require("intersection"),
         Point = require("point"),
         Rect = require("rect"),
         Road = require("road"),
