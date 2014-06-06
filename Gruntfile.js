5d4f43e9d3ba99afc0685a666bcea3e5896cae0e
Artem Volkhin <artem@volkhin.com>
2014-06-06 11:21:15 +0400

    add google closure linter

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,56 +1,77 @@
 /* global module */
 module.exports = function(grunt) {
-    "use strict";
+  'use strict';
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON("package.json"),
-        requirejs: {
-            compile: {
-                options: {
-                    baseUrl: "./js",
-                    mainConfigFile: "./js/main.js",
-                    name: "main",
-                    out: "./dist/app.js",
-                    optimize: "none",
-                },
-            },
-        },
-        jshint: {
-            files: ["Gruntfile.js", "js/**/*.js", "spec/*.js"],
-            options: {
-                jshintrc: true,
-            },
-        },
-        jasmine: {
-            requirejsTemplate: {
-                src: "js/**/*.js",
-                options: {
-                    specs: "spec/*Spec.js",
-                    helpers: "spec/*Helper.js",
-                    template: require("grunt-template-jasmine-requirejs"),
-                    templateOptions: {
-                        requireConfigFile: "js/main.js",
-                    },
-                },
-            },
-        },
-        watch: {
-            scripts: {
-                files: ["js/**/*.js", "spec/*.js"],
-                tasks: ["jshint", "jasmine"],
-                options: {
-                    spawn: false,
-                },
-            },
-        },
-    });
+  var sources = ['*.js', 'js/**/*.js', 'spec/**/*.js'];
 
-    grunt.loadNpmTasks("grunt-contrib-jshint");
-    grunt.loadNpmTasks("grunt-contrib-requirejs");
-    grunt.loadNpmTasks("grunt-contrib-jasmine");
-    grunt.loadNpmTasks("grunt-contrib-watch");
-    grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    requirejs: {
+      compile: {
+        options: {
+          baseUrl: './js',
+          mainConfigFile: './js/main.js',
+          name: 'main',
+          out: './dist/app.js',
+          optimize: 'none'
+        }
+      }
+    },
+    jshint: {
+      files: sources,
+      options: {
+        jshintrc: true
+      }
+    },
+    jasmine: {
+      requirejsTemplate: {
+        src: 'js/**/*.js',
+        options: {
+          specs: 'spec/*Spec.js',
+          helpers: 'spec/*Helper.js',
+          template: require('grunt-template-jasmine-requirejs'),
+          templateOptions: {
+            requireConfigFile: 'js/main.js'
+          }
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: sources,
+        tasks: ['jshint', 'jasmine'],
+        options: {
+          spawn: false
+        }
+      }
+    },
+    closureLint: {
+      options: {
+        strict: true
+      },
+      app: {
+        src: sources,
+        command: 'gjslint'
+      }
+    },
+    closureFixStyle: {
+      options: {
+        strict: true
+      },
+      app: {
+        src: sources,
+        command: 'fixjsstyle'
+      }
+    }
+  });
 
-    grunt.registerTask("default", ["jshint", "jasmine"]);
-    grunt.registerTask("test", ["jshint", "jasmine"]);
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-closure-linter');
+
+  grunt.registerTask('default', ['jshint', 'jasmine']);
+  grunt.registerTask('test', ['jshint', 'jasmine']);
 };
